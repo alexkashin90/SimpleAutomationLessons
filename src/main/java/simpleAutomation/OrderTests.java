eaea191308c52fa8740e185deb44a8bfb5938b14
Aleksandr Kashin <alexkashin.90@gmail.com>
2021-04-23 00:14:20 +0300

    made some tests and changed POM

--- src/main/java/simpleAutomation/OrderTests.java
+++ src/main/java/simpleAutomation/OrderTests.java
@@ -1,13 +1,45 @@
 package simpleAutomation;
 
-public class OrderTests extends BaseTestWithOrder{
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+public class OrderTests{
+
+    @BeforeAll
+    public static void createOrder() {
+        System.out.println("create order");
+    }
+
+    @Test
     public void testCreateOrder (){
         System.out.println("check if order was created");
     }
 
+    @Test
     public void testListOfOrders(){
         System.out.println("list of orders");
         System.out.println("check if orders are displayed");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"manager of group", "manager of department", "manager of branch",
+    "CEO", "Founder"})
+    public void testListOfOrdersAsManager(String role){
+        System.out.println("list of orders as " + role);
+        System.out.println("check if orders are displayed for " + role);
+    }
+
+    @AfterEach
+    public void deleteOrder(){
+        System.out.println("delete order");
+    }
+
+    @AfterAll
+    public static void deleteAllOrders(){
+        System.out.println("delete all orders");
     }
 }
