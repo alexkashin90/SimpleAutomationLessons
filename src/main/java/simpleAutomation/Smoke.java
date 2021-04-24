dda59a64301fdf30ed7bb2552d7ee01ad900dcd1
Aleksandr Kashin <alexkashin.90@gmail.com>
2021-04-24 21:55:34 +0300

    added annotation and two more files

---
+++ src/main/java/simpleAutomation/Smoke.java
@@ -0,0 +1,16 @@
+package simpleAutomation;
+
+
+import org.junit.jupiter.api.Tag;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("Smoke")
+public @interface Smoke {
+
+}
